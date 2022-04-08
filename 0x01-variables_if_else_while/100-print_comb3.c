cc0aaa5343982f27fa72618a581429f0cd92bd8a
root <aleazaryilma@outlook.com>
2022-04-08 09:49:18 -0700

    task 10 white spac fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,7 +3,7 @@
 /**
  * main - Prints all possible combinations of two different digits,
  *        in ascending order, separated by a comma followed by a space.
- * 
+ *
  * Return: Always 0.
  */
 int main(void)
