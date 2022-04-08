a0bb5fc545496a5d827deb92e45c085a7400ee07
root <aleazaryilma@outlook.com>
2022-04-08 05:36:39 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *       
+ * Description: Print the lowercase alphabet
+ * Return: Always 0.
+ */
+int main(void)
+{
+int c;
+
+for (c = 0; c < 26; ++c)
+putchar('a' + c);
+
+putchar('\n');
+
+return (0);
+}
