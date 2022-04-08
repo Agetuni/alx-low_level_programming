4745e90a166d653ea7aec111ac601da2010ee9df
root <aleazaryilma@outlook.com>
2022-04-08 09:44:52 -0700

    task 10 fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+
 /**
  * main - Prints all possible combinations of two different digits,
- *        in ascending order, separated by a comma followed by a space
+ *        in ascending order, separated by a comma followed by a space.
  * 
  * Return: Always 0.
  */
