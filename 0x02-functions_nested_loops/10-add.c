09123def7b7d039f3217b2f6df77e12a852e2f35
root <aleazaryilma@outlook.com>
2022-04-12 06:40:13 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * Main - unction that adds two integers and returns the result.
+ * 
+ *
+ * Return: Void.
+ */
+ int void(int n, int m) 
+{
+int sum = n + m;
+_printf(sum);
+}
