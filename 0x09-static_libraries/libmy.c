cad2b2d181d562c4205ebe7e57a9875d93c92ace
root <aleazaryilma@outlook.com>
2022-04-29 05:31:01 -0700

    libmy.a

--- 0x09-static_libraries/libmy.c
+++ 0x09-static_libraries/libmy.c
@@ -4,6 +4,53 @@
  *
  *
  */
-int main(){
+int _putchar(char c) {
 	return 0;
+}
+int _islower(int c) {
+	return 0;
+}
+int _isalpha(int c) {
+	return 0;
+}
+int _abs(int n) {
+	return 0;
+}
+int _isupper(int c) {
+	return 0;
+}
+int _isdigit(int c) {
+	return 0;
+}
+int _strlen(char* s) {
+	return 0;
+}
+void _puts(char* s) {
+}
+char* _strcpy(char* dest, char* src) {
+	return "Hello";
+}
+int _atoi(char* s) {
+	return 0;
+}
+char* _strcat(char* dest, char* src) {
+	return "Hello";
+}
+char* _strncat(char* dest, char* src, int n) {
+	return "Hello";
+}
+char* _strncpy(char* dest, char* src, int n) {
+	return "Hello";
+}
+int _strcmp(char* s1, char* s2) {
+	return 0;
+}
+char* _memset(char* s, char b, unsigned int n) {
+	return "Hello";
+}
+char* _memcpy(char* dest, char* src, unsigned int n) {
+	return "Hello";
+}
+char* _strchr(char* s, char c) {
+	return "Hello";
 }
