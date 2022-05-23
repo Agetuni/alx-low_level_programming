f41b4d6768a28f72c41670b5b9ed380e9212dfa8
Aleazar yilma <yilma4060@gmail.com>
2022-05-23 19:39:09 +0300

    advanced task

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,10 +1,7 @@
-#ifndef _LIST_H_
-#define _LIST_H_
-#include <stdio.h>
-#include <stddef.h>
-#include <stdlib.h>
+#ifndef LISTS_H
+#define LISTS_H
 #include <string.h>
-
+#include <stdio.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -34,5 +31,4 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-
 #endif
