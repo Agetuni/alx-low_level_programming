d67fe82eeb9e3b28413fab8321ca235053e5012f
Aleazar yilma <yilma4060@gmail.com>
2022-07-07 19:30:16 +0300

    Fix task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,46 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - the function
+ * main - check the code
  *
- * @h: the pointer
- * @idx: the node wanted
- * @n: data
- * Return: new node
+ * Return: Always EXIT_SUCCESS.
  */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+int main(void)
 {
-	/* allow memory*/
-	dlistint_t *prev, *new_node = new_node;
-	unsigned int i = 1;
+    dlistint_t *head;
 
-	new_node = malloc(sizeof(dlistint_t));
-
-	/* set prev head*/
-	prev = (*h);
-	/* in case of fail*/
-	if (new_node == NULL)
-	{
-		return (NULL);
-	}
-	if ((*h) == NULL)
-	{
-		return (NULL);
-	}
-	/* set next of newNode*/
-	while (i <= idx)
-	{
-		if (i == idx)
-		{
-			new_node->n = n;
-			new_node->next = prev->next;
-			prev->next = new_node;
-			new_node->prev = prev;
-			new_node->next->prev = new_node;
-			return (new_node);
-		}
-		prev = prev->next;
-		i++;
-	}
-	return (NULL);
+    head = NULL;
+    add_dnodeint_end(&head, 0);
+    add_dnodeint_end(&head, 1);
+    add_dnodeint_end(&head, 2);
+    add_dnodeint_end(&head, 3);
+    add_dnodeint_end(&head, 4);
+    add_dnodeint_end(&head, 98);
+    add_dnodeint_end(&head, 402);
+    add_dnodeint_end(&head, 1024);
+    print_dlistint(head);
+    printf("-----------------\n");
+    insert_dnodeint_at_index(&head, 5, 4096);
+    print_dlistint(head);
+    free_dlistint(head);
+    head = NULL;
+    return (EXIT_SUCCESS);
 }
