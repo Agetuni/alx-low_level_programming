5f4a184ede3fedbe316ec75851f3806779163682
Aleazar yilma <yilma4060@gmail.com>
2022-07-14 22:32:15 +0300

    task 18

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,7 +1,5 @@
-#ifndef DYNLIB
-#define DYNLIB
-
-#include <stdlib.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -24,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif 
+#endif
